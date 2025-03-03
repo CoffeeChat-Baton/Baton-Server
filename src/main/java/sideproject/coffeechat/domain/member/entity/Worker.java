737884b9aebe1ab60e77909f1b7aa438dae772d0
hajungIm <133209775+hajungIm@users.noreply.github.com>
2025-03-04 03:02:24 +0900

    Feat : Worker 엔티티 생성
    - Member를 상속받음

---
+++ src/main/java/sideproject/coffeechat/domain/member/entity/Worker.java
@@ -0,0 +1,36 @@
+package sideproject.coffeechat.domain.member.entity;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+
+@Getter
+@Entity
+@DiscriminatorValue("WORKER")
+public class Worker extends Member {
+
+    private Long jobId;
+
+    private String jobName;
+
+    private Long subJobId;
+
+    private String subJobName;
+
+    private int careerYears;
+
+    public void updateJob(Long jobId, String jobName) {
+        this.jobId = jobId;
+        this.jobName = jobName;
+    }
+
+    public void updateSubJob(Long subJobId, String subJobName) {
+        this.subJobId = subJobId;
+        this.subJobName = subJobName;
+    }
+
+    public void updateCareerYears(int careerYears) {
+        this.careerYears = careerYears;
+    }
+
+}
