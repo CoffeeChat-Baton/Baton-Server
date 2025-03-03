9e74fd201d8bf192b0e9fc5f52ac9bfd13495dc3
hajungIm <133209775+hajungIm@users.noreply.github.com>
2025-03-04 03:10:55 +0900

    Feat : Worker 등록 Service 구현
    - 등록 전 Job, SubJob 검증
      - SubJob이 Job에 종속되어 있는지
      - '기타' 선택 시 customName이 들어있는지

---
+++ src/main/java/sideproject/coffeechat/domain/member/service/WorkerService.java
@@ -0,0 +1,92 @@
+package sideproject.coffeechat.domain.member.service;
+
+import static sideproject.coffeechat.global.response.code.ErrorCode.JOB_NOT_FOUND;
+import static sideproject.coffeechat.global.response.code.ErrorCode.JOB_REQUEST_ERROR;
+import static sideproject.coffeechat.global.response.code.ErrorCode.SUB_JOB_NOT_FOUND;
+import static sideproject.coffeechat.global.response.code.ErrorCode.WORKER_NOT_FOUND;
+
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sideproject.coffeechat.domain.member.dto.converter.JobConverter;
+import sideproject.coffeechat.domain.member.dto.request.WorkerJoinRequest;
+import sideproject.coffeechat.domain.member.dto.response.JobResponse;
+import sideproject.coffeechat.domain.member.dto.response.SubJobResponse;
+import sideproject.coffeechat.domain.member.entity.Job;
+import sideproject.coffeechat.domain.member.entity.Member;
+import sideproject.coffeechat.domain.member.entity.SubJob;
+import sideproject.coffeechat.domain.member.entity.Worker;
+import sideproject.coffeechat.domain.member.repository.JobRepository;
+import sideproject.coffeechat.domain.member.repository.SubJobRepository;
+import sideproject.coffeechat.domain.member.repository.WorkerMapper;
+import sideproject.coffeechat.domain.member.repository.WorkerRepository;
+import sideproject.coffeechat.global.response.code.ErrorCode;
+import sideproject.coffeechat.global.response.exception.MemberException;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class WorkerService {
+
+    private static final String ETC = "기타";
+
+    private final MemberService memberService;
+    private final WorkerRepository workerRepository;
+    private final WorkerMapper workerMapper;
+    private final JobRepository jobRepository;
+    private final SubJobRepository subJobRepository;
+
+    public void join(WorkerJoinRequest request, String username) {
+        Member member = memberService.getMemberByUsername(username);
+        Map<String, String> jobNames = validateAndGetCustomJobNames(
+                request.getJobId(), request.getCustomJobName(),
+                request.getSubJobId(), request.getCustomSubJobName()
+        );
+        workerMapper.joinWorker(
+                member.getId(),
+                request.getNickname(),
+                request.getJobId(), jobNames.get("jobName"),
+                request.getSubJobId(), jobNames.get("subJobName"),
+                request.getCareerYears(),
+                request.getEmail(),
+                request.getProfileImageUrl()
+        );
+    }
+
+
+    public Map<String, String> validateAndGetCustomJobNames(Long jobId, String customJobName,
+                                                            Long subJobId, String customSubJobName) {
+        Job job = jobRepository.findById(jobId)
+                .orElseThrow(() -> new MemberException(JOB_NOT_FOUND));
+        SubJob subJob = subJobRepository.findById(subJobId)
+                .orElseThrow(() -> new MemberException(SUB_JOB_NOT_FOUND));
+
+        if (!subJob.getJob().getId().equals(jobId)) {
+            throw new MemberException(JOB_REQUEST_ERROR, "This SubJob does not belong to this Job");
+        }
+
+        return validateCustomJobNames(job, customJobName, subJob, customSubJobName);
+    }
+
+    private static Map<String, String> validateCustomJobNames(Job job, String customJobName,
+                                                              SubJob subJob, String customSubJobName) {
+        boolean isJobEtc = job.getJobName().equals(ETC);
+        boolean isSubJobEtc = subJob.getSubJobName().equals(ETC);
+
+        if (isJobEtc && customJobName == null) {
+            throw new MemberException(JOB_REQUEST_ERROR, "CustomJobName is required");
+        }
+        if (isSubJobEtc && customSubJobName == null) {
+            throw new MemberException(JOB_REQUEST_ERROR, "CustomSubJobName is required");
+        }
+
+        return Map.of(
+                "jobName", isJobEtc ? customJobName  : job.getJobName(),
+                "subJobName", isSubJobEtc ? customSubJobName : subJob.getSubJobName()
+        );
+    }
+}
