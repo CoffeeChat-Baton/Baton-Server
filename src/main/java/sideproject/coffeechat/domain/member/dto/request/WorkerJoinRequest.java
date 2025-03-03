14342d6bb564a06f9ea57b1846913d51eeb339c5
hajungIm <133209775+hajungIm@users.noreply.github.com>
2025-03-04 03:23:15 +0900

    Feat : Worker 등록 Request 생성
    - @WorkerOnly 검증 어노테이션 구현

---
+++ src/main/java/sideproject/coffeechat/domain/member/dto/request/WorkerJoinRequest.java
@@ -0,0 +1,43 @@
+package sideproject.coffeechat.domain.member.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sideproject.coffeechat.domain.member.entity.MemberType;
+import sideproject.coffeechat.global.validator.WorkerOnly;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class WorkerJoinRequest {
+
+    @NotNull
+    @WorkerOnly
+    private MemberType memberType;
+
+    @NotNull
+    private String nickname;
+
+    @NotNull
+    private Long jobId;
+
+    // Job 이 "기타"인 경우만 필요
+    private String customJobName;
+
+    @NotNull
+    private Long subJobId;
+
+    // SubJob 이 "기타"인 경우만 필요
+    private String customSubJobName;
+
+    @NotNull
+    private Integer careerYears;
+
+    @Email
+    private String email;
+
+    private String profileImageUrl;
+
+}
