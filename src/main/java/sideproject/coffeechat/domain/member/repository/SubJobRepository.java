e038a3c8a879f6bed0b733cead9f4be69894b9aa
hajungIm <133209775+hajungIm@users.noreply.github.com>
2025-03-04 03:04:25 +0900

    Feat : SubJob 엔티티 생성

---
+++ src/main/java/sideproject/coffeechat/domain/member/repository/SubJobRepository.java
@@ -0,0 +1,10 @@
+package sideproject.coffeechat.domain.member.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import sideproject.coffeechat.domain.member.entity.SubJob;
+
+@Repository
+public interface SubJobRepository extends JpaRepository<SubJob, Long> {
+}
