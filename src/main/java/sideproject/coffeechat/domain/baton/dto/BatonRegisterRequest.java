7e67f3e49710c5461fc280ee06c4ff9ed871799c
hajungIm <133209775+hajungIm@users.noreply.github.com>
2025-03-06 03:45:29 +0900

    Feat : 바통 등록 Controller 구현
    - 바통 등록 Request DTO 생성

---
+++ src/main/java/sideproject/coffeechat/domain/baton/dto/BatonRegisterRequest.java
@@ -0,0 +1,56 @@
+package sideproject.coffeechat.domain.baton.dto;
+
+import jakarta.persistence.Column;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sideproject.coffeechat.domain.baton.entity.Charge;
+import sideproject.coffeechat.global.validator.HasPriority;
+import sideproject.coffeechat.global.validator.TimeRangeProvider;
+import sideproject.coffeechat.global.validator.UniquePriority;
+import sideproject.coffeechat.global.validator.ValidStartEndTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BatonRegisterRequest {
+
+    @NotNull
+    private Charge charge;
+
+    @NotNull
+    @Size(max = 3)
+    @Valid
+    @UniquePriority(message = "TimeSlot priorities must be unique.")
+    private List<TimeSlotRequest> availableSchedules;
+
+    @Column(nullable = false)
+    private String preQuestion;
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @ValidStartEndTime
+    public static class TimeSlotRequest implements HasPriority, TimeRangeProvider {
+
+        @NotNull
+        private LocalDate date;
+
+        @NotNull
+        private LocalTime startTime;
+
+        @NotNull
+        private LocalTime endTime;
+
+        @NotNull
+        private int priority;
+
+    }
+
+}
