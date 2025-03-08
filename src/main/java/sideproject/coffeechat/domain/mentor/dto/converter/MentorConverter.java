a5054a443b2d7232cce3c6091184ef4da1a8b19d
hajungIm <133209775+hajungIm@users.noreply.github.com>
2025-03-08 19:15:15 +0900

    Refactor : 멘토 상세 조회 Service 수정
    - 쿼리로 필요한 db 정보 한번에 조회

--- src/main/java/sideproject/coffeechat/domain/mentor/dto/converter/MentorConverter.java
+++ src/main/java/sideproject/coffeechat/domain/mentor/dto/converter/MentorConverter.java
@@ -1,10 +1,9 @@
 package sideproject.coffeechat.domain.mentor.dto.converter;
 
-import java.math.BigDecimal;
-import java.util.Comparator;
 import java.util.List;
-import sideproject.coffeechat.domain.member.entity.Worker;
-import sideproject.coffeechat.domain.mentor.dto.CompactMentorDTO;
+import sideproject.coffeechat.domain.mentor.dto.mapperdto.CompactMentorDTO;
+import sideproject.coffeechat.domain.mentor.dto.mapperdto.MentorDTO;
+import sideproject.coffeechat.domain.mentor.dto.mapperdto.MentorDTO.MentorTimeSlotDTO;
 import sideproject.coffeechat.domain.mentor.dto.request.MentorRegisterRequest.TimeSlotRequest;
 import sideproject.coffeechat.domain.mentor.dto.response.CompactMentorResponse;
 import sideproject.coffeechat.domain.mentor.dto.response.MentorResponse;
@@ -27,39 +26,6 @@ public class MentorConverter {
                 }).toList();
     }
 
-    public static MentorResponse toMentorResponse(
-            Mentor mentor, Worker worker, Long mentoringCount, BigDecimal responseRate
-    ) {
-        return MentorResponse.builder()
-                .mentorId(mentor.getId())
-                .profileImageUrl(mentor.getWorker().getProfileImageUrl())
-                .nickname(mentor.getWorker().getNickname())
-                .companyName(mentor.getCompanyName())
-                .jobName(worker.getJobName())
-                .subJobName(worker.getSubJobName())
-                .careerYears(worker.getCareerYears())
-                .mentoringCount(mentoringCount)
-                .responseRate(responseRate)
-                .shortDescription(mentor.getShortDescription())
-                .detailedDescription(mentor.getDetailedDescription())
-                .availableSchedules(toMentorTimeSlotResponses(mentor.getAvailableSchedules()))
-                .build();
-    }
-
-    private static List<MentorTimeSlotResponse> toMentorTimeSlotResponses(List<MentorTimeSlot> timeSlots) {
-        return timeSlots.stream()
-                .sorted(Comparator.comparing(MentorTimeSlot::getPriority))
-                .map(timeSlot -> {
-                    return new MentorTimeSlotResponse(
-                            timeSlot.getId(),
-                            timeSlot.getAvailableDays(),
-                            timeSlot.getStartTime(),
-                            timeSlot.getEndTime(),
-                            timeSlot.getPriority()
-                    );
-                }).toList();
-    }
-
     public static List<CompactMentorResponse> toCompactMentorResponses(List<CompactMentorDTO> dtos) {
         return dtos.stream()
                 .map(dto -> {
@@ -75,6 +41,37 @@ public class MentorConverter {
                             .mentoringCount(dto.getMentoringCount())
                             .responseRate(dto.getResponseRate())
                             .isBookmarked(dto.isBookmarked())
+                            .build();
+                }).toList();
+    }
+
+    public static MentorResponse toMentorResponse(MentorDTO dto) {
+        return MentorResponse.builder()
+                .mentorId(dto.getMentorId())
+                .profileImageUrl(dto.getProfileImageUrl())
+                .nickname(dto.getNickname())
+                .companyName(dto.getCompanyName())
+                .jobName(dto.getJobName())
+                .subJobName(dto.getSubJobName())
+                .careerYears(dto.getCareerYears())
+                .mentoringCount(dto.getMentoringCount())
+                .responseRate(dto.getResponseRate())
+                .shortDescription(dto.getShortDescription())
+                .detailedDescription(dto.getDetailedDescription())
+                .availableSchedules(toMentorTimeSlotResponse(dto.getAvailableSchedules()))
+                .isBookmarked(dto.isBookmarked())
+                .build();
+    }
+
+    private static List<MentorTimeSlotResponse> toMentorTimeSlotResponse(List<MentorTimeSlotDTO> dtos) {
+        return dtos.stream()
+                .map(dto -> {
+                    return MentorTimeSlotResponse.builder()
+                            .timeSlotId(dto.getTimeSlotId())
+                            .availableDays(dto.getAvailableDays())
+                            .startTime(dto.getStartTime())
+                            .endTime(dto.getEndTime())
+                            .priority(dto.getPriority())
                             .build();
                 }).toList();
     }
