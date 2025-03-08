6c582e0c07de86970490dee5f99962229617419a
hajungIm <133209775+hajungIm@users.noreply.github.com>
2025-03-08 21:48:15 +0900

    Refactor : ChatBaton으로 타입 캐스트

--- src/main/java/sideproject/coffeechat/domain/baton/service/BatonService.java
+++ src/main/java/sideproject/coffeechat/domain/baton/service/BatonService.java
@@ -6,9 +6,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import sideproject.coffeechat.domain.baton.dto.BatonConverter;
-import sideproject.coffeechat.domain.baton.dto.BatonRegisterRequest;
-import sideproject.coffeechat.domain.baton.entity.Baton;
-import sideproject.coffeechat.domain.baton.entity.BatonTimeSlot;
+import sideproject.coffeechat.domain.baton.dto.ChatBatonRegisterRequest;
+import sideproject.coffeechat.domain.baton.entity.ChatBaton;
+import sideproject.coffeechat.domain.baton.entity.ChatTimeSlot;
 import sideproject.coffeechat.domain.baton.repository.BatonRepository;
 import sideproject.coffeechat.domain.member.entity.Member;
 import sideproject.coffeechat.domain.member.service.MemberService;
@@ -26,18 +26,18 @@ public class BatonService {
     private final S3Uploader s3Uploader;
     private final BatonRepository batonRepository;
 
-    public void register(Long mentorId, BatonRegisterRequest request, MultipartFile attachment, String username) {
+    public void registerChatBaton(Long mentorId, ChatBatonRegisterRequest request, MultipartFile attachment, String username) {
         Member mentee = memberService.getMemberByUsername(username);
         Mentor mentor = mentorService.findByMentorId(mentorId);
 
         String attachmentUrl = uploadAttachment(attachment, mentee);
-        saveBaton(request, attachmentUrl, mentor, mentee);
+        saveChatBaton(request, attachmentUrl, mentor, mentee);
     }
 
-    private void saveBaton(BatonRegisterRequest request, String attachmentUrl, Mentor mentor, Member mentee) {
-        Baton baton = BatonConverter.toBaton(request, attachmentUrl, mentor, mentee);
-        Baton savedBaton = batonRepository.save(baton);
-        List<BatonTimeSlot> availableSchedules = BatonConverter
+    private void saveChatBaton(ChatBatonRegisterRequest request, String attachmentUrl, Mentor mentor, Member mentee) {
+        ChatBaton chatBaton = BatonConverter.toChatBaton(request, attachmentUrl, mentor, mentee);
+        ChatBaton savedBaton = (ChatBaton) batonRepository.save(chatBaton);
+        List<ChatTimeSlot> availableSchedules = BatonConverter
                 .toBatonTimeSlotList(savedBaton, request.getAvailableSchedules());
         savedBaton.updateAvailableSchedules(availableSchedules);
     }
