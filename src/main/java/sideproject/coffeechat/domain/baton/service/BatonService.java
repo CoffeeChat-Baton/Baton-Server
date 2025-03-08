ac86a45450d55147306d6b51f919ea0ed1f58d46
hajungIm <133209775+hajungIm@users.noreply.github.com>
2025-03-09 01:20:36 +0900

    Feat : 포트폴리오, 이력서 바통 등록 Service 구현

--- src/main/java/sideproject/coffeechat/domain/baton/service/BatonService.java
+++ src/main/java/sideproject/coffeechat/domain/baton/service/BatonService.java
@@ -5,10 +5,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-import sideproject.coffeechat.domain.baton.dto.BatonConverter;
-import sideproject.coffeechat.domain.baton.dto.ChatBatonRegisterRequest;
+import sideproject.coffeechat.domain.baton.dto.converter.BatonConverter;
+import sideproject.coffeechat.domain.baton.dto.request.ChatBatonRegisterRequest;
+import sideproject.coffeechat.domain.baton.dto.request.PortfolioBatonRegisterRequest;
+import sideproject.coffeechat.domain.baton.dto.request.ResumeBatonRegisterRequest;
 import sideproject.coffeechat.domain.baton.entity.ChatBaton;
 import sideproject.coffeechat.domain.baton.entity.ChatTimeSlot;
+import sideproject.coffeechat.domain.baton.entity.PortfolioBaton;
+import sideproject.coffeechat.domain.baton.entity.ResumeBaton;
 import sideproject.coffeechat.domain.baton.repository.BatonRepository;
 import sideproject.coffeechat.domain.member.entity.Member;
 import sideproject.coffeechat.domain.member.service.MemberService;
@@ -26,7 +30,9 @@ public class BatonService {
     private final S3Uploader s3Uploader;
     private final BatonRepository batonRepository;
 
-    public void registerChatBaton(Long mentorId, ChatBatonRegisterRequest request, MultipartFile attachment, String username) {
+    public void registerChatBaton(
+            String username, Long mentorId, ChatBatonRegisterRequest request, MultipartFile attachment
+    ) {
         Member mentee = memberService.getMemberByUsername(username);
         Mentor mentor = mentorService.findByMentorId(mentorId);
 
@@ -38,13 +44,35 @@ public class BatonService {
         ChatBaton chatBaton = BatonConverter.toChatBaton(request, attachmentUrl, mentor, mentee);
         ChatBaton savedBaton = (ChatBaton) batonRepository.save(chatBaton);
         List<ChatTimeSlot> availableSchedules = BatonConverter
-                .toBatonTimeSlotList(savedBaton, request.getAvailableSchedules());
+                .toChatTimeSlots(savedBaton, request.getAvailableSchedules());
         savedBaton.updateAvailableSchedules(availableSchedules);
     }
 
     private String uploadAttachment(MultipartFile attachment, Member mentee) {
         String prefix = mentee.getId() + "/baton/";
         return s3Uploader.upload(prefix, attachment);
+    }
+
+    public void registerPortfolioBaton(
+            String username, Long mentorId, PortfolioBatonRegisterRequest request, MultipartFile attachment
+    ) {
+        Member mentee = memberService.getMemberByUsername(username);
+        Mentor mentor = mentorService.findByMentorId(mentorId);
+
+        String attachmentUrl = uploadAttachment(attachment, mentee);
+        PortfolioBaton portfolioBaton = BatonConverter.toPortfolioBaton(request, attachmentUrl, mentor, mentee);
+        batonRepository.save(portfolioBaton);
+    }
+
+    public void registerResumeBaton(
+            String username, Long mentorId, ResumeBatonRegisterRequest request, MultipartFile attachment
+    ) {
+        Member mentee = memberService.getMemberByUsername(username);
+        Mentor mentor = mentorService.findByMentorId(mentorId);
+
+        String attachmentUrl = uploadAttachment(attachment, mentee);
+        ResumeBaton resumeBaton = BatonConverter.toResumeBaton(request, attachmentUrl, mentor, mentee);
+        batonRepository.save(resumeBaton);
     }
 
 }
