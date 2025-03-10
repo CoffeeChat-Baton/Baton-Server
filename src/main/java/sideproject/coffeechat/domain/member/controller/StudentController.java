e5aee485ee66894a285dd787fdbfd2a5ec0f762f
hajungIm <133209775+hajungIm@users.noreply.github.com>
2025-03-10 10:28:15 +0900

    Feat : 학력, 전공 조회 엔드포인트 생성

--- src/main/java/sideproject/coffeechat/domain/member/controller/StudentController.java
+++ src/main/java/sideproject/coffeechat/domain/member/controller/StudentController.java
@@ -1,14 +1,19 @@
 package sideproject.coffeechat.domain.member.controller;
 
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import sideproject.coffeechat.domain.member.dto.request.StudentJoinRequest;
+import sideproject.coffeechat.domain.member.dto.response.EducationResponse;
+import sideproject.coffeechat.domain.member.dto.response.MajorResponse;
 import sideproject.coffeechat.domain.member.service.StudentService;
 import sideproject.coffeechat.global.response.Response;
 
@@ -29,4 +34,14 @@ public class StudentController {
         return Response.success();
     }
 
+    @GetMapping("/education-list")
+    public Response<List<EducationResponse>> getEducations() {
+        return Response.success(studentService.getEducations());
+    }
+
+    @GetMapping("/major-list/{educationId}")
+    public Response<List<MajorResponse>> getSubJobsByJobId(@PathVariable Long educationId) {
+        return Response.success(studentService.getMajorsByEducationId(educationId));
+    }
+    
 }
---
+++ src/main/java/sideproject/coffeechat/domain/member/dto/response/EducationResponse.java
@@ -0,0 +1,16 @@
+package sideproject.coffeechat.domain.member.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EducationResponse {
+
+    private Long educationId;
+
+    private String educationName;
+
+}
